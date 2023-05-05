94e8c4003fae106d8b0e2c5a75afbdb814f8ad8e
Danny Gim <danny.gim@zealot.co.jp>
2023-05-05 16:54:00 +0900

    use twind v1

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,9 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig } from "@twind/core";
+import presetTailwind from "@twind/preset-tailwind";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind()],
+  }),
   selfURL: import.meta.url,
-} as Options;
+};
